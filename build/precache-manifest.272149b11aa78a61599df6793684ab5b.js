94ad0c71c3d1406ffa48c3b9b9aec347fd5e4532
Ramahesitro <64641757+Ramahesitro@users.noreply.github.com>
2020-05-05 20:21:45 +0200

    adding starting project

---
+++ build/precache-manifest.272149b11aa78a61599df6793684ab5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a56dce8fadb99e0768501e869a7214",
+    "url": "/Ramahesitro/robofriends.git/index.html"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/Ramahesitro/robofriends.git/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "6285853f2bef10aa7830",
+    "url": "/Ramahesitro/robofriends.git/static/css/main.d02d0768.chunk.css"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/Ramahesitro/robofriends.git/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ramahesitro/robofriends.git/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6285853f2bef10aa7830",
+    "url": "/Ramahesitro/robofriends.git/static/js/main.bb8da374.chunk.js"
+  },
+  {
+    "revision": "4c88854e17ca6ceaf7c7",
+    "url": "/Ramahesitro/robofriends.git/static/js/runtime-main.3b902a75.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Ramahesitro/robofriends.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
